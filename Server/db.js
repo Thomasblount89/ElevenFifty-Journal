241e474f637422299a64a0b6cbf3cb95f372363c
Thomas Blount <Thomasblount89@gmail.com>
2021-02-26 17:57:47 -0500

    Ejournal draft

---
+++ Server/db.js
@@ -0,0 +1,6 @@
+     const Sequelize = require('sequelize');
+     
+     const sequelize = new Sequelize("postgres://postgres:Frankie83@localhost:5432/eleven-journal");
+   
+     module.exports = sequelize; 
+
